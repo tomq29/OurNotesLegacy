c35e14293b6c84b0469181ac2c26ac485f4cef22
Tamirlan <striped.pricier.0k@icloud.com>
2024-08-08 15:18:46 +0300

    first commit

---
+++ server/demo.ts
@@ -0,0 +1,22 @@
+type Test = {
+  id: number;
+  name: string;
+  str: string;
+};
+
+const obj: Test = {
+  id: 1,
+  name: 'Tom',
+  str: '1',
+};
+
+function sum(str: string): number {
+  obj.str = str;
+
+  return +str;
+}
+
+
+const 
+
+console.log(obj, );
