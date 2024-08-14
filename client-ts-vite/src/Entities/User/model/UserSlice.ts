5e31902e8a8b8734a8c8a5e40a360910b336aeae
Tamirlan <striped.pricier.0k@icloud.com>
2024-08-14 21:47:21 +0300

    lika expain my trouble

---
+++ client-ts-vite/src/Entities/User/model/UserSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+initialState
+
+
+const userSlice = createSlice({
+
+    name: "currentUser",
+    initialState,
+    reducers:{},
+    extraReducers:(builder)=> {
+
+
+    }
+
+})
